e320ca99569004be6b5cf87edb024c023b5f9db9
wp <wp@LAPTOP-KBV9GIBS.mshome.net>
2021-06-30 18:11:02 -0300

    3° Commit

--- Pratica02-GIT/src/HelloWorld.java
+++ Pratica02-GIT/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
 		System.out.println("Primeiros testes com o Git.");
+		System.out.println("Disciplina: Gerência de Configuração e mudança de software.");
 
 	}
 
